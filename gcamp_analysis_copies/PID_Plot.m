97387b89b02754a085d4e3d4893395fcf24f89f1
katieshakman <katieshak@gmail.com>
2015-08-26 14:52:53 -0400

    Replaced softlink with recent copies of what I needed from imaging analysis code.

---
+++ gcamp_analysis_copies/PID_Plot.m
@@ -0,0 +1,25 @@
+% PID_Plot
+clear all; close all; 
+
+% Variables user can change: 
+x_limits = [2000,7000];
+%% Import data
+PID_file = uigetfile('*.csv');
+% importer interface --> importable range A2:B1001
+% variables user can alter:
+rowOffset = 1; 
+colOffset = 0;
+%
+M = csvread(PID_file,rowOffset,colOffset);
+%% Plot data
+fig = figure;
+Mx = M(:,1); 
+My = M(:,2);
+plot(Mx,My)
+xlabel('Time')
+ylabel('PID Reading')
+set(gca, 'xlim', x_limits)
+%% Save figure
+figName = ['PID_',PID_file(1:end-4)];
+saveas(fig, figName, 'fig'); 
+saveas(fig, figName, 'png'); 
---
+++ gcamp_analysis_copies/PlotAll_DelFoverF_ofType.m
@@ -0,0 +1,224 @@
+%% Plot Only Fluor Analysis
+% Plot from previously stored deltaF/F data and timepoints. 
+clear all; close all;
+
+StartDir = pwd; 
+
+% Variables user can change: 
+x_limits = [2,7];
+%% Load data files from several user-selected directories. 
+imDir = uigetdir(); % Allow user to select directory. 
+cd(imDir);
+currentDirectory = pwd; % For use in save paths. 
+
+all_delFoverF_files = dir('*delFoverF.mat');
+all_timePoints_files = dir('*timePoints.mat');
+all_baseline_files = dir('*baselineF0.mat'); 
+dFoF = [];
+tPts = [];
+F0s = []; 
+stringsList = []; % initialize
+% dFoF = zeros(1, max(size(all_delFoverF_files))); % Initialize
+% tPts = zeros(1, max(size(all_timePoints_files))); % Initialize
+pattern = '._' ; 
+for idx = 1:max(size(all_delFoverF_files))
+%     new_delFoverF = load(all_delFoverF_files(idx,1).name);
+    notTrueMAT = strfind(all_delFoverF_files(idx,1).name, pattern);
+    if notTrueMAT
+        ; % do nothing for now.  maybe delete these files in future. 
+    else
+        load(all_delFoverF_files(idx,1).name);
+        dFoF = [delFoverF; dFoF];
+        stringsList = [all_delFoverF_files(idx,1).name(9:25); stringsList];
+        % used only part of the name that contains date and tseries number
+    end
+end
+stringsList = cellstr(stringsList); % make cell array list of strings
+clear idx;
+for idx = 1:max(size(all_timePoints_files))
+    notTrueMAT = strfind(all_timePoints_files(idx,1).name, pattern);
+    if notTrueMAT
+        ; % do nothing for now.  maybe delete these files in future. 
+    else
+        load(all_timePoints_files(idx,1).name);
+        tPts = [timePoints; tPts]; 
+    end
+end
+clear idx;
+for idx = 1:max(size(all_baseline_files)) % also get baselines
+    notTrueMAT = strfind(all_baseline_files(idx,1).name, pattern);
+    if notTrueMAT
+        ; % do nothing for now.  maybe delete these files in future. 
+    else
+        load(all_baseline_files(idx,1).name);
+        F0s = [F_0; F0s]; 
+    end
+end
+clear idx; 
+clear pattern;
+
+% Assert that all trials being plotted together were acquired with the same
+% time points (same number of frames and length of period).
+if size(tPts,1) > 1
+    assert(max(max(tPts) - min(tPts)) == 0,...
+        'Expect all timePts to be the same') ;
+end
+%% Plot GCaMP (for each trial and on average) versus time. 
+% framePeriod = 0.818748; % Frame period : seconds/frame. 
+% framePeriod fetched automatically from config file, stored as framePerVal. 
+% timePoints = [ 1:maxTime ] * framePeriod; 
+fig = figure;
+xvalsOn = [3,3];
+xvalsOff = [5,5];
+% plot(timePoints, CaAvg);
+% plot(timePoints, delFoverF);
+subplot(1,2,1); % trial-by-trial plotting. 
+hold all
+evokedTrialSums = zeros(size(dFoF, 1),1); % initialize 
+for idx = 1:max(size(dFoF, 1))
+    plot(tPts(1,:), dFoF(idx,:));
+    %
+    ind = 0; 
+    for i = 1:length(timePoints)
+        if (timePoints(i) > 3) && (timePoints(i) < 5)
+            ind = ind +1;
+            evokedTrialSums(idx) = evokedTrialSums(idx) + dFoF(idx, i); 
+            denom = ind; 
+        end
+    end
+end
+% set limits of x axis
+set(gca,'xlim', x_limits);
+
+evokedTrialAvgs = evokedTrialSums/denom; 
+%
+%legend('show')
+legend(stringsList) % "creates a legend in the current axes and uses the entries in strings to label each set of data. Specify strings as either a cell array of strings or a matrix of strings."
+% mean(A,1) is a row vector containing the mean value of each column.
+avg_dFoF = mean(dFoF,1);
+max_dFoF = max(dFoF);
+min_dFoF = min(dFoF);
+
+% % Calculate and set max value for y axis. 
+% % dFoF_max = max(max(dFoF));
+% % avg_dFoF_max = max(max(avg_dFoF));
+% % set_y_max = max(dFoF_max, avg_dFoF_max);
+
+title('deltaF/F vs Time')
+% Annotate the point (3, delFoverF(3))
+% text(3, delFoverF(3),'\leftarrow (delFoverF(3sec))',...
+%      'HorizontalAlignment','left')
+% title('GCaMP Average vs Time')
+% % Add a line at timepoint = 3 sec
+    yvals = get(gca, 'ylim');
+    max_y_vals = yvals
+    
+    lineOn = plot(xvalsOn, yvals);
+    lineOff = plot(xvalsOff, yvals);
+    % Make the new vertical line at 3s a dash-dotted black line. 
+    set(lineOn,'Color','k','LineWidth', 1, 'LineStyle','-.')
+    set(lineOff,'Color','k','LineWidth', 1, 'LineStyle','-.')
+hold off
+% Also plot the mean of each column (timePt):
+
+subplot(1,2,2);
+avg_dFoF_plot = plot(tPts, avg_dFoF);
+set(avg_dFoF_plot, 'Color', 'blue', 'LineWidth', 1)
+
+hold on
+title('Average deltaF/F vs Time')
+
+% Annotate the point (3, delFoverF(3))
+% % % text(3, delFoverF(3),'\leftarrow (delFoverF(3sec))',...
+% % %      'HorizontalAlignment','left');
+
+% % % Add max and min curves.
+% % maxCurve = plot(tPts(1,:), max_dFoF);
+% % set(maxCurve, 'Color', 'blue', 'LineWidth', 1, 'LineStyle', '--');
+% % minCurve = plot(tPts(1,:), min_dFoF);
+% % set(minCurve, 'Color', 'blue', 'LineWidth', 1, 'LineStyle', '--');
+%
+% How to fill in space between curves (not working properly):
+% curveDiffVec = [min_dFoF, max_dFoF];
+% xCurve = [timePoints,timePoints];
+% fill(xCurve, curveDiffVec, 'b'); % In the style of: http://www.mathworks.com/matlabcentral/answers/14005
+% Add a line at timepoint = 3 sec
+    set(gca, 'ylim', max_y_vals);
+    yvals = get(gca, 'ylim');
+%     xvalsOn = [3,3];
+    lineOn = plot(xvalsOn, yvals);
+%     xvalsOff = [4,4];
+    lineOff = plot(xvalsOff, yvals);
+    % Make the new vertical line at 3s a dash-dotted black line. 
+    set(lineOn,'Color','k','LineWidth', 1, 'LineStyle','-.')
+     % Make the new vertical line at 4s a dash-dotted black line.
+    set(lineOff,'Color','k','LineWidth', 1, 'LineStyle','-.')
+    legend
+hold off
+set(gca,'xlim', x_limits);
+
+%% Save figure files and evokedAvg value
+dateStart = strfind(imDir, '201'); 
+dateEnd = dateStart-1 + min(strfind(imDir(dateStart:end), '/'))-1; 
+date = imDir(dateStart:dateEnd);
+trialStart = max(strfind(imDir, '/'))+1; 
+trialID = imDir(trialStart:end); 
+figName = [date,'_', trialID];
+%savePath = currentDirectory(...
+            %strfind(currentDirectory, 'Raw_Un')+16:strfind(...
+            %currentDirectory, 'TSeries')-1);
+%figName = savePath; 
+saveas(fig, figName, 'fig'); 
+saveas(fig, figName, 'png'); 
+
+ind = 0; 
+
+for i = 1:length(timePoints)
+    if (timePoints(i) > 3) && (timePoints(i) < 5)
+        ind = ind +1;
+        evokedTimes(ind) = timePoints(i);
+        evokedResp(ind) = avg_dFoF(i);
+    end
+end
+
+% convert currentDirectory to currentDirName
+currentDirName = strrep(currentDirectory, '/', '_')  % Print to console.
+
+evokedAvg = mean(evokedResp); 
+evokedAvg_savefile = [figName, '_evokedAvg']; 
+save(evokedAvg_savefile, 'evokedAvg'); 
+load(evokedAvg_savefile); 
+%
+evokedPeak = max(evokedResp); 
+evokedPeak_savefile = [figName, '_evokedPeak']; 
+save(evokedPeak_savefile, 'evokedPeak'); 
+load(evokedPeak_savefile); 
+%
+avgBaseline = mean(F0s); 
+avgBaseline_savefile = [figName, '_avgBaseline'];
+save(avgBaseline_savefile, 'avgBaseline'); 
+load(avgBaseline_savefile); 
+
+%display('evokedAvg ='); 
+evokedAvg
+%display('evokedPeak ='); 
+evokedPeak
+%
+avgBaseline
+%
+evokedTrialAvgs % display values
+
+
+avg_dFoF_savefile = [figName, '_avg_dFoF'];
+save(avg_dFoF_savefile, 'avg_dFoF'); 
+
+% save a copy in the GroupedData folder)
+savepath = '/Volumes/KATIELAB1/Data_Analysis/GroupData/' ;
+cd(savepath);
+save(evokedAvg_savefile, 'evokedAvg');
+save(avg_dFoF_savefile, 'avg_dFoF', 'tPts'); 
+% save(fullfile(savepath,evokedAvg_savefile,'evokedAvg'));
+% save(fullfile(savepath,avg_dFoF_savefile,'avg_dFoF'));
+%save(avg_dFoF_savefile, 'avg_dFoF'); 
+
+cd(StartDir); 
